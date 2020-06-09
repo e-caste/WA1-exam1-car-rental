56c091891eb1d85abc45de7507f9828e7fd784f1
e-caste-mbp <en.cas.dev@icloud.com>
2020-06-09 17:24:52 +0200

    add /user APIs

--- server/server.js
+++ server/server.js
@@ -10,6 +10,28 @@ const app = express();
 app.use(morgan('tiny'));
 app.use(express.json())
 
+const prefix = "/api";
 
+// REST APIs - always begin with /api
+
+// /user APIs
+
+// /user/login
+// POST
+// request: {username, password}
+// response:
+//  404 - user not found
+//  401 - wrong password
+//  200 - login successful
+
+// /user/logout
+// POST
+// request: none
+// response:
+//  200 - logout successful
+
+// /car APIs
+
+// /rental APIs
 
 app.listen(port, () => console.log(`Server running on http://localhost:${port}/`));
