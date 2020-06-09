11842f949606fbbb268bd85289ea0bca1e86bc01
e-caste-mbp <en.cas.dev@icloud.com>
2020-06-09 22:28:51 +0200

    add car_dao.js

---
+++ server/dao/car_dao.js
@@ -0,0 +1,36 @@
+'use strict';
+
+import db from "../db";
+import Car from "../entities/car";
+
+const createCar = row => new Car(row.id, row.brand, row.model,  // required
+                                row.description, row.kilometers, row.year, row.fuel, // optional
+                                row.value, row.kmperlitre, row.passengers, row.stickshift === 1);  // optional
+
+exports.getCars = function () {
+    return new Promise((resolve, reject) => {
+        const sql = "SELECT * FROM CARS";
+        db.all(sql, [], (err, rows) => {
+            if (err)
+                reject(err);
+            else if (rows.length === 0)
+                resolve(undefined);
+            else
+                resolve(rows.map(r => createCar(r)));
+        });
+    });
+}
+
+exports.getCar = function (carId) {
+    return new Promise((resolve, reject) => {
+        const sql = "SELECT * FROM CARS WHERE id = ?";
+        db.all(sql, [carId], (err, rows) => {
+            if (err)
+                reject(err);
+            else if (rows.length === 0)
+                resolve(undefined);
+            else
+                resolve(createCar(rows[0]));
+        });
+    });
+}
