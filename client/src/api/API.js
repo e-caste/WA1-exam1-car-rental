5dcba668f98d6d11272c98325afa56441f1fb3e2
e-caste-mbp <en.cas.dev@icloud.com>
2020-06-11 17:09:45 +0200

    add login function to client-side API

--- client/src/api/API.js
+++ client/src/api/API.js
@@ -11,3 +11,24 @@ async function isLoggedIn() {
     throw {status: res.status, message: "Authentication required"};
 }
 
+async function login(email, password) {
+    return new Promise((resolve, reject) => {
+        fetch(prefix + "/user/login",
+            {
+                method: "POST",
+                headers: {"Content-Type": "application/json"},
+                body: JSON.stringify({email: email, password: password})
+            })
+            .then(res => {
+                if (res.ok)
+                    res.json().then(user => resolve(user));
+                else if (res.status === 404)
+                    reject("Email not found")
+                else if (res.status === 401)
+                    reject("Invalid password")
+                else  // should not happen, but if it does:
+                    reject("Server error")
+            })
+            .catch(err => reject("Server unavailable"))
+    });
+}
