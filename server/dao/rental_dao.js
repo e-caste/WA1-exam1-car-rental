c40331127f50599daf64bb2fdbaececb2025f9d3
e-caste-mbp <en.cas.dev@icloud.com>
2020-06-11 14:47:19 +0200

    remove unnecessary console.log call

--- server/dao/rental_dao.js
+++ server/dao/rental_dao.js
@@ -13,7 +13,6 @@ exports.toggleCanceled = function (rentalId, userId) {
         // check if the given user has made the rental they attempt to modify, if not resolve to undefined to trigger 404
         let sql = "SELECT * FROM RENTALS WHERE id = ? AND userid = ?";
         db.all(sql, [rentalId, userId], (err, rows) => {
-            console.log(rows)
             if (rows.length === 0)
                 resolve(undefined);
         });
