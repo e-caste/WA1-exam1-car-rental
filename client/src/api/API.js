90cf6b5744b3419085a32a58bfe51d3914ca4046
e-caste-mbp <en.cas.dev@icloud.com>
2020-06-11 19:14:40 +0200

    refactor isLoggedIn client-side API function to conform to other functions' format

--- client/src/api/API.js
+++ client/src/api/API.js
@@ -6,11 +6,19 @@ const prefix = "/api";
 // User related APIs
 
 async function isLoggedIn() {
-    const res = await fetch(prefix + "/user");
-    const json = await res.json();
-    if (res.ok)
-        return json;
-    throw {status: res.status, message: "Authentication required"};
+    return new Promise((resolve, reject) => {
+        fetch(prefix + "/user")
+            .then(res => {
+                if (res.ok)
+                    res.json()
+                        .then(json => resolve(json))
+                else if (res.status === 401)
+                    reject("Authentication error");
+                else
+                    reject("Server error")
+            })
+            .catch(err => reject("Server unavailable"));
+    });
 }
 
 async function login(email, password) {
