fcda0f19abe0d306fb026afc2d4b84c038c1cf0c
e-caste-mbp <en.cas.dev@icloud.com>
2020-06-13 11:05:22 +0200

    set cars state with React hooks in App component

--- client/src/App.js
+++ client/src/App.js
@@ -1,17 +1,25 @@
-import React from 'react';
+import React, {useState, useEffect} from 'react';
 
+import API from "./api/API";
 import Header from "./components/Header";
 import {AuthContext} from "./auth/AuthContext";
 
 
 const App = () => {
-  return (
+
+    const [cars, setCars] = useState([]);
+    useEffect(() => {
+        API.getAllCars()
+            .then(cars => setCars(cars))
+    }, []);
+
+    return (
     <div className="App">
         <AuthContext.Provider value={{authUser: {}}}>
             <Header/>
         </AuthContext.Provider>
     </div>
-  );
+    );
 }
 
 export default App;
