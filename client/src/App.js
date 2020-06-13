bac40a63fb9b07b99325627884d768e544c880b2
e-caste-mbp <en.cas.dev@icloud.com>
2020-06-13 10:38:05 +0200

    add Header component to App content

--- client/src/App.js
+++ client/src/App.js
@@ -1,9 +1,15 @@
 import React from 'react';
 
-function App() {
+import Header from "./components/Header";
+import {AuthContext} from "./auth/AuthContext";
+
+
+const App = () => {
   return (
     <div className="App">
-
+        <AuthContext.Provider value={{authUser: {}}}>
+            <Header/>
+        </AuthContext.Provider>
     </div>
   );
 }
