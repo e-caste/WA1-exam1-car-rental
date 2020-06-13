dee06b0b57241e8336d001885fc12b7a29aa16bd
e-caste-mbp <en.cas.dev@icloud.com>
2020-06-13 11:14:01 +0200

    call API to set authUser state

--- client/src/App.js
+++ client/src/App.js
@@ -7,7 +7,15 @@ import {AuthContext} from "./auth/AuthContext";
 
 const App = () => {
 
+    const [authUser, setAuthUser] = useState({});
     const [cars, setCars] = useState([]);
+
+    // initial API calls, note deps=[] to only call once like componentDidMount
+    useEffect(() => {
+        API.isLoggedIn()
+            .then(user => setAuthUser(user))
+    }, []);
+
     useEffect(() => {
         API.getAllCars()
             .then(cars => setCars(cars))
@@ -15,7 +23,7 @@ const App = () => {
 
     return (
     <div className="App">
-        <AuthContext.Provider value={{authUser: {}}}>
+        <AuthContext.Provider value={{authUser: authUser}}>
             <Header/>
         </AuthContext.Provider>
     </div>
