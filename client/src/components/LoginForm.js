912ea94c330e7b79c82ab4ee6610b3f3969e6f7a
e-caste-mbp <en.cas.dev@icloud.com>
2020-06-14 10:45:45 +0200

    replace AuthContext.Consumer with useContext hook in LoginForm component

--- client/src/components/LoginForm.js
+++ client/src/components/LoginForm.js
@@ -1,12 +1,16 @@
-import React, {useState} from "react";
+import React, {useContext, useState} from "react";
 import {Button, Col, Form, Jumbotron, Row} from "react-bootstrap";
 import {Redirect} from "react-router-dom";
 import {AuthContext} from "../auth/AuthContext";
 
 const LoginForm = props => {
 
+    // state variables
     const [email, setEmail] = useState("");
     const [password, setPassword] = useState("");
+
+    // context variable
+    const context = useContext(AuthContext);
 
     const handleChange = event => {
         const t = event.target;
@@ -28,56 +32,54 @@ const LoginForm = props => {
         handleLogin(email, password);
     }
 
-    return(
-        <AuthContext.Consumer>
-            {context =>
-                context.authUser ?
-                    <Redirect to={"/"}/> :
-                    (<div id={"LoginForm"}>
-                        <Jumbotron>
-                            <h1>Login</h1>
-                        </Jumbotron>
-                        <Form
-                            method={"POST"}
-                            onChange={handleChange}
-                            onSubmit={event => handleSubmit(event, context.handleLogin)}
-                        >
-                            <Form.Group controlId="form-email">
-                                <Form.Control
-                                    type="email"
-                                    placeholder="Email"
-                                    value={email}
-                                    required
-                                    autoFocus
-                                />
-                                <Form.Text className="text-muted">
-                                    We'll never share your email with anyone else.
-                                </Form.Text>
-                            </Form.Group>
-                            <Form.Group controlId="form-password">
-                                <Form.Control
-                                    type="password"
-                                    placeholder="Password"
-                                    value={password}
-                                    required
-                                />
-                            </Form.Group>
-                            <Row>
-                                <Col>
-                                    <Button variant="primary" type="submit" className={"pl-10"}>
-                                        Login
-                                    </Button>
-                                </Col>
-                                <Col>
-                                    <Button href={"/resetpassword"} variant={"link"}>
-                                        Forgot your password?
-                                    </Button>
-                                </Col>
-                            </Row>
-                        </Form>
-                    </div>)
+    return(<div>
+            {context.authUser ?
+                <Redirect to={"/"}/> :
+                (<div id={"LoginForm"}>
+                    <Jumbotron>
+                        <h1>Login</h1>
+                    </Jumbotron>
+                    <Form
+                        method={"POST"}
+                        onChange={handleChange}
+                        onSubmit={event => handleSubmit(event, context.handleLogin)}
+                    >
+                        <Form.Group controlId="form-email">
+                            <Form.Control
+                                type="email"
+                                placeholder="Email"
+                                value={email}
+                                required
+                                autoFocus
+                            />
+                            <Form.Text className="text-muted">
+                                We'll never share your email with anyone else.
+                            </Form.Text>
+                        </Form.Group>
+                        <Form.Group controlId="form-password">
+                            <Form.Control
+                                type="password"
+                                placeholder="Password"
+                                value={password}
+                                required
+                            />
+                        </Form.Group>
+                        <Row>
+                            <Col>
+                                <Button variant="primary" type="submit" className={"pl-10"}>
+                                    Login
+                                </Button>
+                            </Col>
+                            <Col>
+                                <Button href={"/resetpassword"} variant={"link"}>
+                                    Forgot your password?
+                                </Button>
+                            </Col>
+                        </Row>
+                    </Form>
+                </div>)
             }
-        </AuthContext.Consumer>
+        </div>
     );
 }
 
