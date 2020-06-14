2d5b20e506c3d911629136c3d084de54a9d99012
e-caste-mbp <en.cas.dev@icloud.com>
2020-06-14 11:23:35 +0200

    use useContext hook in Header component, use handleLogout method from context

--- client/src/components/Header.js
+++ client/src/components/Header.js
@@ -1,35 +1,38 @@
 // this component is called Header to prevent collisions with react-bootstrap's Navbar component
 
-import React from "react";
+import React, {useContext} from "react";
 import {AuthContext} from "../auth/AuthContext";
 import {Navbar, Nav, NavDropdown} from "react-bootstrap";
 
-const Header = props =>
-    <AuthContext.Consumer>
-        {context => (
-            <Navbar bg={"warning"} collapseOnSelect expand={"md"} sticky={"top"}>
-                <Navbar.Brand href={"/"}>
-                    <img src={"back-to-the-rental.svg"} width={30} height={30}  alt={"logo"}/>
-                    {' '}Back to the rental
-                </Navbar.Brand>
+const Header = props => {
 
-                    <div className="ml-auto">
-                        {!context.authUser &&
-                            <Nav.Link href={"/login"}>Login</Nav.Link>
-                        }
-                        {context.authUser &&
-                            <Nav>
-                                <NavDropdown title={`Welcome ${context.authUser.name || "user"}!`} id={"UserDropdown"}>
-                                    <NavDropdown.Item href={"/rent"}>Rent a car</NavDropdown.Item>
-                                    <NavDropdown.Item href={"/rentals"}>Your rentals</NavDropdown.Item>
-                                    <NavDropdown.Divider />
-                                    <NavDropdown.Item href={"/logout"}>Logout</NavDropdown.Item>
-                                </NavDropdown>
-                            </Nav>
-                        }
-                    </div>
-            </Navbar>
-        )}
-    </AuthContext.Consumer>
+    const {authUser, authErr, handleLogin, handleLogout} = useContext(AuthContext);
+
+    return (
+        <Navbar bg={"warning"} collapseOnSelect expand={"md"} sticky={"top"}>
+            <Navbar.Brand href={"/"}>
+                <img src={"back-to-the-rental.svg"} width={30} height={30} alt={"logo"}/>
+                {' '}Back to the rental
+            </Navbar.Brand>
+
+            <div className="ml-auto">
+                {!authUser &&
+                <Nav.Link href={"/login"}>Login</Nav.Link>
+                }
+                {authUser &&
+                <Nav>
+                    <NavDropdown title={`Welcome ${authUser.name || "user"}!`} id={"UserDropdown"}>
+                        <NavDropdown.Item href={"/rent"}>Rent a car</NavDropdown.Item>
+                        <NavDropdown.Item href={"/rentals"}>Your rentals</NavDropdown.Item>
+                        <NavDropdown.Divider/>
+                        <NavDropdown.Item href={"/logout"} onClick={handleLogout}>Logout</NavDropdown.Item>
+                    </NavDropdown>
+                </Nav>
+                }
+            </div>
+        </Navbar>
+    );
+
+}
 
 export default Header;
