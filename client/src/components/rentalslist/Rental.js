cf4942a78850a5aaff1c323e1da056d4f3b3b6be
e-caste-mbp <en.cas.dev@icloud.com>
2020-06-15 18:35:00 +0200

    add Rental component to show in RentalsList at /rental route

---
+++ client/src/components/rentalslist/Rental.js
@@ -0,0 +1,66 @@
+import React, {useState} from "react";
+import {Button} from "react-bootstrap";
+import API from "../../api/API";
+
+
+const Rental = props => {
+
+    const convertDriversAge = age => {
+        switch (age) {
+            case 0:
+                return "under 25";
+            case 1:
+                return "between 26 and 64";
+            case 2:
+                return "over 65";
+            default:
+                console.error("Unexpected driversAge value in carslist.Car");
+                break;
+        }
+    }
+
+    const convertEstimatedKilometers = km => {
+        switch (km) {
+            case 0:
+                return "< 50 km";
+            case 1:
+                return "between 50 and 150 km";
+            case 2:
+                return "> 150 km";
+            default:
+                console.error("Unexpected kmPerDay value in carslist.Car");
+                break;
+        }
+    }
+
+    return (
+        <tr>
+            <td>{props.rental.startingDay}</td>
+            <td>{props.rental.endDay}</td>
+            <td>{props.rental.carCategory}</td>
+            <td>{convertDriversAge(props.rental.driversAge)}</td>
+            <td>{props.rental.extraDrivers === 1 ? "Yes" : "No"}</td>
+            <td>{convertEstimatedKilometers(props.rental.estimatedKilometers)}</td>
+            <td>{props.rental.insurance === 1 ? "Yes" : "No"}</td>
+            <td>{props.rental.amount}</td>
+            {props.cancel &&
+            (!props.rental.canceled ?
+            <td>
+                <Button
+                    variant={"outline-danger"}
+                    onClick={() => props.cancel(props.rental)}
+                >
+                    Cancel
+                </Button>
+            </td> :
+            <td>Yes</td>)
+            }
+            {!props.cancel &&
+            <td>{props.rental.canceled ? "Yes" : "No"}</td>
+            }
+        </tr>
+    );
+}
+
+
+export default Rental;
