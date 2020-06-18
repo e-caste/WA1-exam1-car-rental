289fb7613823136eae004ed40812cd21575b3395
e-caste-mbp <en.cas.dev@icloud.com>
2020-06-18 11:35:50 +0200

    use useHistory hook to go back to /login route in ResetPasswordForm

--- client/src/components/ResetPasswordForm.js
+++ client/src/components/ResetPasswordForm.js
@@ -1,22 +1,45 @@
 import React from "react";
-import {Navbar} from "react-bootstrap";
+import {Button, Navbar, Row} from "react-bootstrap";
+import {useHistory} from "react-router-dom";
 
-const ResetPasswordForm = props =>
-    <div id={"ResetPasswordForm"} className={"text-center  ml-6 mr-6"}>
-        <img src={"sloth.svg"} height={200} alt={""} />
-        <h4>You may be interested in the <a href={"https://www.top10homeremedies.com/home-remedies/home-remedies-improve-memory.html"}>
-                 top 10 home remedies to improve memory</a>.
-        </h4>
-        <Navbar fixed={"bottom"} className={"justify-content-center"}>
-            <Navbar.Text>
-                <h6 className={"text-muted"}>
-                    I am not in any way affiliated with the website linked above.
-                    <br />
-                    This page was made as a joke since we are not required to set
-                    up a mail server within this project.
-                </h6>
-            </Navbar.Text>
-        </Navbar>
-    </div>
+const ResetPasswordForm = props => {
+
+    const {push} = useHistory();
+
+    return (
+        <div id={"ResetPasswordForm"}>
+            <Row className={"justify-content-center"}>
+                <img
+                    src={"sloth.svg"}
+                    height={200}
+                    alt={""}
+                />
+                <h4 className={"mt-5"}>
+                    You may be interested in the <a
+                    href={"https://www.top10homeremedies.com/home-remedies/home-remedies-improve-memory.html"}>
+                    top 10 home remedies to improve memory</a>.
+                </h4>
+            </Row>
+            <Row className={"mt-4 justify-content-center"}>
+                <Button
+                    variant={"outline-primary"}
+                    onClick={() => push("/login")}
+                >
+                    Go back?
+                </Button>
+            </Row>
+            <Navbar fixed={"bottom"} className={"text-center justify-content-center"}>
+                <Navbar.Text>
+                    <h6 className={"text-muted"}>
+                        I am not in any way affiliated with the website linked above.
+                        <br/>
+                        This page was made as a joke since we are not required to set
+                        up a mail server within this project.
+                    </h6>
+                </Navbar.Text>
+            </Navbar>
+        </div>
+    );
+}
 
 export default ResetPasswordForm;
