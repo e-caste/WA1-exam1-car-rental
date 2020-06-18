1a1cf78087f0499b7b1ec31719b2c2b6b882db64
e-caste-mbp <en.cas.dev@icloud.com>
2020-06-18 20:03:33 +0200

    add Footer component

---
+++ client/src/components/Footer.js
@@ -0,0 +1,19 @@
+import React from "react";
+import {Container, Navbar} from "react-bootstrap";
+
+const Footer = props =>
+    <div id={"Footer"}
+         className={"mt-auto"}
+    >
+        <Navbar
+            sticky={"bottom"}
+            variant={"light"}
+        >
+            <Navbar.Text className={"ml-auto mr-auto"}>Contact us</Navbar.Text>
+            <Navbar.Text className={"ml-auto mr-auto"}>Our prices</Navbar.Text>
+            <Navbar.Text className={"ml-auto mr-auto"}>Our locations</Navbar.Text>
+            <Navbar.Text className={"ml-auto mr-auto"}>© 2020 Enrico Castelli</Navbar.Text>
+        </Navbar>
+    </div>
+
+export default Footer;
