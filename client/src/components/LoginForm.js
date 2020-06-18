3f3f90d6d663e1f07ac70d3a33c8d6c46cf02d07
e-caste-mbp <en.cas.dev@icloud.com>
2020-06-18 15:19:13 +0200

    add id to jumbotron in LoginForm

--- client/src/components/LoginForm.js
+++ client/src/components/LoginForm.js
@@ -37,7 +37,7 @@ const LoginForm = props => {
     return(authUser ?
             <Redirect to={"/"}/> :
             (<div id={"LoginForm"}>
-                <Jumbotron>
+                <Jumbotron id={"jumbotron-loginform"}>
                     <h1>Login</h1>
                 </Jumbotron>
                 <Form
