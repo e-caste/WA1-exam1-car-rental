2b8f199074237b29ae40e0033551a791d115a001
e-caste-mbp <en.cas.dev@icloud.com>
2020-06-18 10:37:15 +0200

    add client luts to server utils

---
+++ server/utils/luts.js
@@ -0,0 +1,11 @@
+exports.kmPerDayLUT = {
+    under50: "less than 50 km",
+    between50and150: "between 50 and 150 km",
+    over150: "over 150 km",
+}
+
+exports.driversAgeLUT = {
+    under25: "under 25",
+    between26and64: "between 26 and 64",
+    over65: "over 65",
+}
