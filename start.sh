dd8915d6696e4e9af52f41d4f3e62a5ff02beb57
e-caste-mbp <en.cas.dev@icloud.com>
2020-06-24 20:41:10 +0200

    fix start.sh script: first launch nodemon, then npm start

--- start.sh
+++ start.sh
@@ -3,16 +3,16 @@
 # exit after any error
 set -e
 
-# start client React development server in the background, after installing dependencies
-echo "Starting client development server..."
-cd client
-npm install
-npm start &
-echo "Client development server started"
-
 # start API server in the foreground, after installing dependencies
 echo "Starting API server..."
-cd ../server
+cd server
 npm install
-nodemon server.js 
+nodemon server.js &
+
+# start client React development server in the background, after installing dependencies
+echo "Starting client development server..."
+cd ../client
+npm install
+npm start 
+echo "Client development server started"
 
